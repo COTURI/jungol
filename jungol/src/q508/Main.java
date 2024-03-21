9c43c89c16340edd216b454ca6308277288c7f0b
21-manic-006g <21-manic-006g@21-manic-006>
2024-03-21 17:33:15 +0900

    go

---
+++ jungol/src/q508/Main.java
@@ -0,0 +1,16 @@
+package q508;
+
+public class Main {
+	public static void main(String[] arg) {
+		System.out.printf("%10s%10s%10s","item","count","price\n");
+		System.out.printf("%10s%10s%10s","pen","20","100\n");
+		System.out.printf("%10s%10s%10s","note","5","95\n");
+		System.out.printf("%10s%10s%10s","eraser","110","97\n");
+	}
+}
+/* 1st
+ 		System.out.println("      item     count     price");
+		System.out.println("       pen        20       100");
+		System.out.println("      note         5        95");
+		System.out.println("    eraser       110        97");
+*/
---
+++ jungol/src/q514/Main.java
@@ -0,0 +1,14 @@
+package q514;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] arg) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("height = ");
+		int height = sc.nextInt();
+		sc.close();
+
+		System.out.print("Your height is "+ height+ "cm.");
+	}
+}
