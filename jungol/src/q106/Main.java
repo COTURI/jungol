eb5f5a206eadcf2b6a82328606dcc19086bbb5de
21-manic-006g <21-manic-006g@21-manic-006>
2024-03-22 12:45:45 +0900

    go

---
+++ jungol/src/q106/Main.java
@@ -0,0 +1,14 @@
+package q106;
+
+public class Main {
+	public static void main(String[] arg) {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+//		int n1,n2,n3;
+//		n1 = 10;
+//		n2 = 20;
+//		n3 = 30;
+		System.out.print(a + " + " + b + " = " + c);
+	}
+}
