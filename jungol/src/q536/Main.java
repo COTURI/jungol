7d14df37624241955b3d008fc21c20fd937f1e4d
21-manic-006g <21-manic-006g@21-manic-006>
2024-03-25 12:53:13 +0900

    go

---
+++ jungol/src/q536/Main.java
@@ -0,0 +1,28 @@
+package q536;
+
+public class Main {
+	public static void main(String[] args) {
+		int i = 1;
+
+		do {
+			System.out.print(i++ + " ");
+		} while (i <= 14);
+		if (i == 15) {
+			System.out.print(i);
+		}
+
+		System.out.println();
+	}
+}
+//	int i=1;
+//	while(i<=15)
+//	{
+//		System.out.print(i + " ");
+//		i++; // = System.out.print(i++ + " ");
+//	}
+//		System.out.println();
+//
+//		for (int i = 1; i <= 15; i++)
+//			System.out.print(i + " ");
+//	}
+//}
