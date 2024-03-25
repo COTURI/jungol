0711153ce37de7bad71b436417b4273c9b67a967
21-manic-006g <21-manic-006g@21-manic-006>
2024-03-25 14:22:57 +0900

    g

--- jungol/src/q594/Main.java
+++ jungol/src/q594/Main.java
@@ -6,9 +6,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String a = sc.nextLine(); // 스캐너 string 일때는 next(공백전 글자까지) or nextLine (한줄전체
+		String a = sc.nextLine(); // 스캐너 string 일때는 next(공백이전 글자까지) or nextLine (한줄전체
 		sc.close();
-
-		System.out.print(a + a);
+		
+		System.out.println(a + a);
+		
+		
 	}
 }
+//		for(int a = 0 ; i<100; i++)
+// 		System.out.print(i)
---
+++ jungol/src/q595/Main.java
@@ -0,0 +1,19 @@
+package q595;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String name = "Hong Gil Dong";
+//		System.out.print(name.charAt(3));
+//		System.out.print(name.charAt(4));
+//		System.out.print(name.charAt(5));
+//		System.out.println(name.charAt(6));
+		
+//		for(int i=3;i<=6;i++) {
+//			System.out.print(name.charAt(i));
+//		}
+		
+		System.out.println(name.substring(3,7));
+	}
+
+}
