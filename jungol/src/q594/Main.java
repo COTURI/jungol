b8c5ec564356ce8f7b686600f6637bc37e7bf7b7
21-manic-006g <21-manic-006g@21-manic-006>
2024-03-25 12:54:20 +0900

    g

--- jungol/src/q594/Main.java
+++ jungol/src/q594/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String a = sc.next();
+		String a = sc.next(); // 스캐너 string 일때는 next or nextline
 		sc.close();
 
 		System.out.print(a + a);
