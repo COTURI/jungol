fdbbf1bf9888f75b0f29589a13d7250e272388e2
21-manic-006g <21-manic-006g@21-manic-006>
2024-03-27 17:46:50 +0900

    g

---
+++ jungol/src/q108/Main.java
@@ -0,0 +1,14 @@
+package q108;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 50;
+		double b = 100.12;
+		double c = a * b;
+
+		System.out.print(b + " * " + a + " = " + (int)c);
+
+	}
+
+}
