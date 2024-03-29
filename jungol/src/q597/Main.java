e518cbaee2a40037586fe1f329ee94806b9dd8e7
21-manic-006g <21-manic-006g@21-manic-006>
2024-03-29 16:35:51 +0900

    g

---
+++ jungol/src/q597/Main.java
@@ -0,0 +1,18 @@
+package q597;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		sc.close();
+		int c = a.length() + b.length();
+
+		System.out.print(c);
+
+	}
+
+}
