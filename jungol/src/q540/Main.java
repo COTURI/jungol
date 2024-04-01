3614bfea0b3e7c05a64c09dc9a1dc9b3c3a07612
21-manic-006g <21-manic-006g@21-manic-006>
2024-04-01 15:27:35 +0900

    g

---
+++ jungol/src/q540/Main.java
@@ -0,0 +1,23 @@
+package q540;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		for (;;) {
+			int a = sc.nextInt();
+			if (a % 3 == 0) {
+				System.out.println(a / 3);
+			}
+			else if (a == -1) {
+				System.out.println();
+				sc.close();
+				break;
+
+			}
+		}
+	}
+}
