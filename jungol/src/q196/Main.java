7960549c4df8c478fa9dd24a26acc04909da6c8a
21-manic-006g <21-manic-006g@21-manic-006>
2024-04-16 17:48:02 +0900

    0416

---
+++ jungol/src/q196/Main.java
@@ -0,0 +1,36 @@
+package q196;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		String tel = sc.next();
+		String addr = sc.next();
+		sc.close();
+
+		Person p1 = new Person(name, tel, addr);
+		p1.print();
+	}
+}
+
+class Person {
+	private String name;
+	private String tel;
+	private String addr;
+
+	Person(String name, String tel, String addr) {
+		this.name = name;
+		this.tel = tel;
+		this.addr = addr;
+	}
+
+	public void print() {
+		System.out.println("name : " + name);
+		System.out.println("tel : " + tel);
+		System.out.println("addr : " + addr);
+	}
+
+}
