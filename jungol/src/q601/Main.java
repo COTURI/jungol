c4fdcd24cafbea39d2ae3947219e6f7b65707244
21-manic-006g <21-manic-006g@21-manic-006>
2024-04-19 10:29:32 +0900

    0419

---
+++ jungol/src/q601/Main.java
@@ -0,0 +1,19 @@
+package q601;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		sc.close();
+		for (int i = 0; i < str.length(); i++) {
+			char anw = str.charAt(str.length() - 1);
+			String name = anw + str.substring(0, str.length() - 1);
+			str = name;
+			System.out.println(name);
+		}
+	}
+}
