c4723d9ee53e29591f05789bd086b264328447c7
21-manic-006g <21-manic-006g@21-manic-006>
2024-05-27 10:33:56 +0900

    g

--- jungol/src/q1840/Main.java
+++ jungol/src/q1840/Main.java
@@ -7,19 +7,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int n = 0;
-		for(;;) {
-
-		for (int i = 0; i < a; i++) {
-			for (int j = 0; j < b; j++) {
-				int c = sc.nextInt();
-				n=c;
-				System.out.print(n + " ");
-			}
-			System.out.println();
-		}}
+		String name = sc.next();
+		String school = sc.next();
+		int grade = sc.nextInt();
+		sc.close();
+		System.out.println("Name : " + name);
+		System.out.println("School : " + school);
+		System.out.println("Grade : " + grade);
 	}
 }
-// 
