1e805277aeb2c18006c761149d1b251064707891
21-manic-006g <21-manic-006g@21-manic-006>
2024-06-03 14:35:07 +0900

    0603

--- jungol/src/q1304/Main.java
+++ jungol/src/q1304/Main.java
@@ -6,14 +6,22 @@ public class Main {
 	public static void main(String arg[]) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
+		int[][] arr = new int[a][a];
+		sc.close();
 		int n = 1;
-		for (int i = 1; i <= a; i++) {
-			for (int j = 1; j <= a; j+=a) {
-				System.out.print(n + " ");
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < a; j++) {
+//				System.out.print(n + " ");
+				arr[i][j] = n;
 				n++;
 			}
+//			System.out.println();
+		}
+		for(int i = 0; i<a; i++) {
+			for(int j = 0; j<a; j++) {
+				System.out.print(arr[j][i] + " ");		
+			}
 			System.out.println();
-			
 		}
 	}
 }
